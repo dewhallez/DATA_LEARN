c1ee3a26cad776f63e7c4eb31ef7bacd014b1ae9
akinwale <4412196+dewhallez@users.noreply.github.com>
2021-03-29 14:17:40 -0400

    used for loops to generate x_labels

--- codes/die_visual.py
+++ codes/die_visual.py
@@ -1,19 +1,33 @@
+import pygal
 from die import Die
 
+
 # Create a D6
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 #print(results)
 
 # Analyze the results
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result):
     frequency = results.count(value)
     frequencies.append(frequency)
-print(frequencies)
+#print(frequencies)
+
+# Visualize the result
+hist = pygal.Bar()
+hist.title = "Result of rolling two D6 dice 1000 times."
+hist.x_labels = [x for x in range(13)]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + D6', frequencies)
+hist.render_to_file('die_vusual3.svg')
 
