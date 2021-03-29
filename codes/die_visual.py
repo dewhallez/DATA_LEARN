29025c15d2c36bd57e3d95dbb1c9d3c8144ed125
akinwale <4412196+dewhallez@users.noreply.github.com>
2021-03-29 14:27:34 -0400

    correct x axis range value

--- codes/die_visual.py
+++ codes/die_visual.py
@@ -8,7 +8,7 @@ die_2 = Die()
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(1000):
+for roll_num in range(10000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 #print(results)
@@ -16,7 +16,7 @@ for roll_num in range(1000):
 # Analyze the results
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
-for value in range(2, max_result):
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 #print(frequencies)
@@ -24,10 +24,10 @@ for value in range(2, max_result):
 # Visualize the result
 hist = pygal.Bar()
 hist.title = "Result of rolling two D6 dice 1000 times."
-hist.x_labels = [x for x in range(13)]
+hist.x_labels = [x for x in range(2, max_result + 1)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
 hist.add('D6 + D6', frequencies)
-hist.render_to_file('die_vusual3.svg')
+hist.render_to_file('die_vusual4.svg')
 
