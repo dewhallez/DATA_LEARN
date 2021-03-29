ca843281c46c1de245ba0b570f89ace4452267ed
akinwale <4412196+dewhallez@users.noreply.github.com>
2021-03-29 18:12:28 -0400

    world_population.py with defaultstyle

--- codes/world_population.py
+++ codes/world_population.py
@@ -2,6 +2,9 @@ import json
 from country_codes import get_country_code
 import pygal_maps_world.maps
 import pygal
+from pygal.style import DefaultStyle
+
+
 # Load the data into a list.
 
 filename = 'population_data.json'
@@ -20,9 +23,27 @@ for pop_dict in pop_data:
         if code:
             cc_populations[code] = population
             
-wm = pygal.maps.world.World()
-wm.title = 'World Population in 2010, by Country'
-wm.add('2010', cc_populations)
 
-wm.render_to_file('figures/world_populations.svg')
+
+# Group the countries into 3 population levels.
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 10000000000:
+        cc_pops_2[cc] = pop 
+    else:
+        cc_pops_3[cc] = pop 
+
+# See how many countries are in each level.
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
+#wm = pygal.maps.world.World()
+wm_style = DefaultStyle
+wm = pygal.maps.world.World(style=wm_style)
+wm.title = 'World Population in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m - 1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+wm.render_to_file('figures/world_populations3.svg')
 
